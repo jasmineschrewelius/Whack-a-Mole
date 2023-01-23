c8f509e3367e68139bcc059671cf17a343881c98
Jasmine Schrewelius <jassersoet@hotmail.com>
2023-01-23 09:53:27 +0100

    Add Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { GameboardComponent } from './gameboard/gameboard.component';
+import { GameService } from './game.service';
+import { MoleComponent } from './mole/mole.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    GameboardComponent,
+    MoleComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
